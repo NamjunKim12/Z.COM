29c718acf35a6f42983cbdf82350e9e56d614ba6
1 <wrasf175@naver.com>
2024-01-05 01:43:45 +0900

    페러렐 라우트 적용

---
+++ src/app/(beforeLogin)/layout.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+import styles from '@/app/page.module.css'
+
+type Props = {
+    children: ReactNode;
+    modal : ReactNode;
+};
+
+
+export default async function HomeLayout({children, modal}: Props){
+    return (
+        // 로그인 이전시 보여줄 루트 레이아웃
+        <div className={styles.container}>
+            {children}
+            {modal}
+        </div>
+    )
+}
