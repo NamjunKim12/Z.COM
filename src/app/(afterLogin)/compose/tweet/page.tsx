b17e917082b0c9d568a2bf75274dfc1f1b6ea9e4
1 <wrasf175@naver.com>
2024-01-09 01:16:34 +0900

    반응형 페이지 디자인 완성

--- src/app/(afterLogin)/compose/tweet/page.tsx
+++ src/app/(afterLogin)/compose/tweet/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+import Home from "@/app/(afterLogin)/home/page";
+
+export default function Page() {
   return (
-    <>
-      트윗 작성 페이지
-    </>
+    <Home />
   )
 }
